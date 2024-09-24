d91fb12c5c9433652996e5237f07cca8606c12b0
Yasith Liyanage <hlyanjana@gmail.com>
2024-09-24 15:19:48 +0530

    modified:   Codes/Q1.cpp

--- Codes/Q1.cpp
+++ Codes/Q1.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
+using namespace std;
 
+
+class Course {
+
+    private : 
+        int courseID;
+        char courseName[20];
+        int creditPoints;
+
+    public : 
+        void setCourseDetails(int cID, char Cname, int cpt);
+        char displayCourseDetails();
+        void setCreditPoints(int cPoints);
+
+};
+
+void Course::setCourseDetails(int cID, char Cname, int cpt){
+
+    CourseID = cID;
+    strcpy(courseName,Cname);
+    creditPoints = cpt;
+
+}
